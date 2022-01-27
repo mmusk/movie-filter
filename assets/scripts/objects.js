f6a1e6da459c927934317345adafcfe67492073e
Muzakkir <mdmuzakkir296@gmail.com>
2022-01-27 11:01:17 +0530

    created: filter movies on search app

---
+++ assets/scripts/objects.js
@@ -0,0 +1,68 @@
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchMovieBtn = document.getElementById("search-btn");
+
+const movies = [];
+
+const renderMovies = (filter = "") => {
+  const movieList = document.getElementById("movie-list");
+
+  if (!movies.length) {
+    movieList.classList.remove("visible");
+    return;
+  } else {
+    movieList.classList.add("visible");
+  }
+
+  movieList.innerHTML = "";
+
+  const filteredMovies = !filter
+    ? movies
+    : movies.filter((movie) => movie.info.title.includes(filter));
+
+  filteredMovies.forEach((movie) => {
+    const movieEl = document.createElement("li");
+    const { info, ...otherProps } = movie;
+    console.log(otherProps);
+    let { getFormattedTitle } = movie;
+    let text = getFormattedTitle.call(movie) + " - ";
+    for (const key in info) {
+      if (key !== "title") {
+        text += `${key}: ${info[key]}`;
+      }
+      movieEl.textContent = text;
+    }
+    movieList.append(movieEl);
+  });
+};
+
+const addMovieHandler = () => {
+  const title = document.getElementById("title").value;
+  const extraName = document.getElementById("extra-name").value;
+  const extraValue = document.getElementById("extra-value").value;
+
+  if (!title.trim() || !extraName.trim() || !extraValue.trim()) {
+    return;
+  }
+
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue,
+    },
+    id: Math.random().toString(),
+    getFormattedTitle() {
+      return this.info.title.toUpperCase();
+    },
+  };
+
+  movies.push(newMovie);
+  renderMovies();
+};
+
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById("filter-title").value;
+  renderMovies(filterTerm);
+};
+
+addMovieBtn.addEventListener("click", addMovieHandler);
+searchMovieBtn.addEventListener("click", searchMovieHandler);
